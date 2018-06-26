16e60b6c980ff62032a89fe2f835f7e9ce000287
Sacchidanand <sacchida222@gmail.com>
2018-06-26 12:00:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# fade_in
+
+Fade in Flutter
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+//import 'package:transparent_image/transparent_image.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Fade in images';
+//assets/images/umbrella.jpg
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+       body: DecoratedBox(decoration: BoxDecoration(
+         image: DecorationImage(
+           image: AssetImage('assets/umbrella.jpg'
+           ),
+         ),
+       ),
+       ),
+      ),
+    );
+  }
+}
